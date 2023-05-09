1ba16dab6a8bb4a0b6eb8ceb17b514bf8988a970
student <you@example.com>
2023-05-09 19:46:08 +0300

    third commit

--- asynchronous.js
+++ asynchronous.js
@@ -14,20 +14,32 @@ showMessage("Welcome to Adalab", 3000);
 // returns a Promise with user data when given a user ID. Write an 
 // asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
 
-
+ async function fetchUserData(userIds) {
+      for (const id of userIds) {
+        try {
+          const userData = await getUserData(id);
+          console.log(userData);
+        } catch (error) {
+          console.error(`Failed to fetch user data for ID ${id}:`, error);
+        }
+      }
+    }
+  let userIds = [015, 001, 056, 076];
+  fetchUserData();
+  
 
 // Question Three
 // You have an asynchronous function performTask() that returns a Promise.
 //  The Promise resolves if the task is successful and rejects if there's an error.
 //  Write a function that calls performTask() and logs a custom success message if 
 // the task is successful, and a custom error message if there's an error.
-
-async function performTask() {
-  try {
+async function performTask(){
+  try{
     await performTask();
-    console.log("Task completed successfully!");
-  } catch (error) {
-    console.error("Error performing task:", error);
+    console.log("Task completd successfully");
+  }
+  catch (error){
+    console.log ("Error performing task:", error);
   }
 }
 performTask();
